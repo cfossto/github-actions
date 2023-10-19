79e46a05be5604913ad8695cd364d6909326d237
Christopher Fossto <70951673+cfossto@users.noreply.github.com>
2023-10-19 12:47:50 +0200

    Update clean_setup.sh

--- clean_setup.sh
+++ clean_setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Setup from a clean environment.
+# Setup from a clean environment. Can be used by an agent to install all dependencies needed on a node.
+# The setup agent needs to run temporarily as a sudoer. -- SHOULD NOT BE A CONVENTIONAL AGENT --
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo adduser jenkins -p ${secrets.JenkinsPassword}
 
 # Clean former installs of Docker
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
@@ -21,6 +27,15 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
+sudo usermod -aG docker jenkins
 
+# # Here we pull all the images needed
 
-# 
+sudo docker pull jenkins
+sudo docker pull atlassian/jira
+
+# Following lines for files that doesn't exist yet.
+# sudo mv ./jenkins_docker_setup.sh /home/jenkins/
+# sudo mv ./jira_docker_setup.sh /home/jenkins
+# sudo su - jenkins -c "sh ./jenkins_docker_setup.sh"
+# sudo su - jenkins -c "sh ./jira_docker_setup.sh"
